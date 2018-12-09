4daf47805746e5e27da7b46b321907f19b45f962
johntestzhang <johntestzhang@yeah.net>
2018-12-09 15:56:51 +0800

    add object manage case and delete some case

--- DailyTest/Libs/CustomKeywords.groovy
+++ DailyTest/Libs/CustomKeywords.groovy
@@ -3,3 +3,7 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
+
+def static "objectManage.baiduObject.searchBox"() {
+    (new objectManage.baiduObject()).searchBox()
+}
